7a926713c6b00b7d60e7b4988a0c20b0f83e0ba0
CelinaPeralta <celper19@bergen.org>
2016-04-02 17:30:25 -0400

    background template

--- PowerBlocker.js
+++ PowerBlocker.js
@@ -2,3 +2,28 @@
 along with courses*/
 /*regularly refresh until criteria (grade) met*/
 
+var SM = (function () {
+
+    var my = {};
+
+    my.get = function (key) {
+        return localStorage.getItem(key);
+    }
+    my.put = function (key, value) {
+        return localStorage.setItem(key, value);
+    }
+    my.delete = function (key) {
+        return localStorage.removeItem(key);
+    }
+    
+    return my;
+
+}());
+
+var PB = (function (SM){
+    
+    
+    
+    
+    
+}(SM)));
