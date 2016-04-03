660b39de7343e86c14ba446378f51fa73c621953
CelinaPeralta <celper19@bergen.org>
2016-04-03 04:44:35 -0400

    almost

--- popup.js
+++ popup.js
@@ -2,18 +2,26 @@ var t = true;
 
 function toggleRedirect() {
 
-/*
-    if () {
-        PB.addBlockedSite();
-    } else {
-        PB.removeBlockedSite();
-    }
-    */
+    /*
+        if () {
+            PB.addBlockedSite();
+        } else {
+            PB.removeBlockedSite();
+        }
+        */
 };
 
-
+function toggle(button) {
+    if (document.getElementById("1").value == "OFF") {
+        document.getElementById("1").value = "ON";
+        document.getElementById("1").style = "color:blue";
+    } else if (document.getElementById("1").value == "ON") {
+        document.getElementById("1").value = "OFF";
+        document.getElementById("1").style = "color:red";
+    }
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('toggle').addEventListener('click', toggleRedirect);
+    document.getElementById('1').addEventListener('click', toggle);
 });
